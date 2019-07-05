15d9e5b93baa8631f4079658cc60ffa315c08100
Sherry030 <52409653+Sherry030@users.noreply.github.com>
2019-07-05 14:13:22 +0800

    在component上传了manage

---
+++ pages/component/manage/check-page.js
@@ -0,0 +1,98 @@
+// pages/component/manage/check-page.js
+
+var texts;
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    items: [
+      { name: '1', value: '常宇乐' },
+      { name: '2', value: '洪锦雯' },
+      { name: '3', value: '程丹静' },
+      { name: '4', value: '关家欢' },
+      { name: '5', value: '郑紫琪' },
+      { name: '6', value: '许书涵' },
+    ],
+    m_project:"羽毛球",
+    snum:"6",
+    rnum:"5"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  
+  formName:function(e){
+    this.setData({
+      name:e.detail.value
+    })
+  },
+
+  checkboxChange: function (e) {
+    var text = [];
+    var id = [];
+    for (var i = 0; i < e.detail.value.length; i++) {
+      var aaa = e.detail.value[i].split(',');
+      text = text.concat(aaa[0])
+      id = id.concat(aaa[1])
+    };
+    texts = text;
+  },
+  submit: function (e) {
+    console.log(texts)
+  }
+})
